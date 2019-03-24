33413d918f6e4868f01309de85080c4c0c586de2
Atilla Ozgur <ati.ozgur@gmail.com>
2019-03-24 21:57:41 +0100

    Create three_five_fifteen.py

---
+++ three_five_fifteen.py
@@ -0,0 +1,10 @@
+
+for i in range(1,101):
+    if (i % 15 == 0):
+        print("fifteen")
+    elif(i % 5 == 0):
+        print("five")
+    elif(i % 5 == 0):        
+        print("five")
+    else:
+        print(i)
